ac58df18f98fbe1f051c68c26e2c5837df08c958
Rekasi Jozsef <resahhdevelop@gmail.com>
2022-01-09 10:49:31 +0100

    Api végleges, csoportok módosítása

--- api/studal/app/Http/Controllers/ClassGroupController.php
+++ api/studal/app/Http/Controllers/ClassGroupController.php
@@ -17,6 +17,13 @@ class ClassGroupController extends Controller
         return Classgroup::create( $request->all() );
     }
 
+    public function update( Request $request, $id ) { 
+        
+        $classgroup = Classgroup::find( $id );
+        $classgroup->update( $request->all() );
+        return $classgroup;
+    }
+
     public function destroy( $id ){
 
         return Classgroup::destroy( $id );
